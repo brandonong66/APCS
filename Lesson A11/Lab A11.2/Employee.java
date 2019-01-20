7a32b8f53201a76856b5570da3e8d0bda8eedba1
SteamedBanana <44516173+SteamedBanana@users.noreply.github.com>
2019-01-19 21:06:02 -0800

    Create Employee.java

---
+++ Lesson A11/Lab A11.2/Employee.java
@@ -0,0 +1,27 @@
+/**
+ * Simulates an hourly employee of a grocery store
+ * @author Brandon Ong
+ * period 3
+ *
+ */
+public  class Employee {
+	private String name;
+	
+	/**
+	 * Constructor
+	 * @param name - clerk's name
+	 */
+	public Employee(String name) {
+		//Your code goes here
+		this.name=name;
+	}
+	
+	/**
+	 * gets the name of the clerk
+	 * @return - clerk name
+	 */
+	public String getName() {
+		//Your code goes here
+		return name;
+	}
+}
