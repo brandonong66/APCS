84d7aa5a1f47e2f7130a76e073369dc7e4a8bd4d
SteamedBanana <44516173+SteamedBanana@users.noreply.github.com>
2019-01-19 20:45:33 -0800

    Add files via upload

---
+++ Lesson A03/src/MathFun.java
@@ -0,0 +1,25 @@
+/** 
+ * 
+ * @author Brandon Ong
+ * Period #3
+ *
+ */
+public class MathFun {
+	/** 
+	 * Method to the calculations and print the answers
+	 */
+	public void calculate()
+	{
+	   int Answer1 = 4+9;
+	   int Answer2 = 46/7;
+	   int Answer3 = 46%7;
+	   int Answer4 = 2*3.0;
+	   
+	   System.out.println("4+9="+ Answer1);
+	   System.out.println("4+9="+ Answer2);
+	   System.out.println("4+9="+ Answer3);
+	   System.out.println("4+9="+ Answer4);
+	   
+		// Complete this method
+	}
+}
---
+++ Lesson A03/src/MathFunDriver.java
@@ -0,0 +1,13 @@
+public class MathFunDriver {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("MathFun Lab 2018-19");
+		MathFun m = new MathFun();
+		m.calculate();
+
+	}
+
+}
